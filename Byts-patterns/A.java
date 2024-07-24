15a54735a92d082efc9c952341394446e580480e
SilverStark18 <amarlearns18215@gmail.com>
2024-07-24 20:16:10 +0530

    changed some code

--- Byts-patterns/A.java
+++ Byts-patterns/A.java
@@ -8,16 +8,16 @@ public class A {
         int n = sc.nextInt();
         int endc = (n + 2) / 2;
         
-        for (int i = 1; i <= n+1; i++) {
-            for (int j = 1; j <= endc; j++) {
-                if (i == 1 ) {
-                    if (j == 1 || j == endc) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == endc) {
+                    if (j == 1 || j == n) {
                         System.out.print(" ");
                     } else {
-                        System.out.print(" *");
+                        System.out.print("*");
                     }
                 } else if (j == 1 || j == n) {
-                    System.out.print(" *");
+                    System.out.print("*");
                 } else {
                     System.out.print(" ");
                 }
