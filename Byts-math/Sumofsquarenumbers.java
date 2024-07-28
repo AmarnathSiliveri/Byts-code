0c457c7694742f14a5a6001c07fcbe62423ddfe4
SilverStar18 <amarlearns18215@gmail.com>
2024-07-28 22:58:26 +0530

    addedsum ofsquarenumbers

---
+++ Byts-math/Sumofsquarenumbers.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+import java.lang.Math;
+
+public class Sumofsquarenumbers
+{
+    public static boolean sqSum(int n) {
+        if (n<=0) return false;
+        else 
+        {
+            for (int i=0;i<=Math.sqrt(n);i++)
+            {
+                int j=n-i*i;
+                int s= (int) Math.sqrt(j);
+                if (s*s == j) ;
+            }
+            return true;
+
+        }
+    }
+    public static void main(String[] args)
+        {
+            System.out.println("Enter a number:");
+            Scanner sc =new Scanner(System.in);
+            int n=sc.nextInt();
+            if (sqSum(n))
+            {
+                System.out.println(n+"can ber epresented as sum of squares numbers");
+
+            }
+            sc.close();
+
+        }
+
+}
